20df6539d6b48a5f1c15231249acc956f08534e5
DLX <768606115@qq.com>
2020-01-30 20:00:23 +0800

    拉普拉斯金字塔图像融合

--- Demo/Pyramids.cpp
+++ Demo/Pyramids.cpp
@@ -84,6 +84,54 @@ void blendLaplacianPyramidsByXYDir(Mat& imageA, Mat& imageB, Mat& imageS) {
 }
 
 // 非顶层融合策略为区域能量
+int G[3][3] = {
+    {1, 2, 1},
+    {2, 4, 2},
+    {1, 2, 1}
+};
+void blendLaplacianPyramidsByRE(Mat& imageA, Mat& imageB, Mat& imageS) {
+    int height = imageA.rows;
+    int width = imageB.cols;
+
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+
+            // 不检查边界
+            if ((i > 1) && (i < (height - 2)) && (j > 1) && (j < (width - 2))) {
+                // 3*3
+                int deltaA[3] = {0};
+                int deltaB[3] = {0};
+                for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+                    for (int colOffset= -1; colOffset <= 1; colOffset++) {
+                        for (int rgb = 0; rgb < 3; rgb++) {
+                            int x = i + rowOffset;
+                            int y = j + colOffset;
+
+                            deltaA[rgb] += G[1+rowOffset][1+colOffset] * imageA.at<Vec3b>(x, y)[rgb];
+                            deltaB[rgb] += G[1+rowOffset][1+colOffset] * imageB.at<Vec3b>(x, y)[rgb];
+                        }
+                    }
+                }
+                // 根据梯度大小进行融合
+                if (deltaA[0] > deltaB[0]) {
+                    imageS.at<Vec3b>(i, j)[0] = imageA.at<Vec3b>(i, j)[0];
+                } else {
+                    imageS.at<Vec3b>(i, j)[0] = imageB.at<Vec3b>(i, j)[0];
+                }
+                if (deltaA[1] > deltaB[1]) {
+                    imageS.at<Vec3b>(i, j)[1] = imageA.at<Vec3b>(i, j)[1];
+                } else {
+                    imageS.at<Vec3b>(i, j)[1] = imageB.at<Vec3b>(i, j)[1];
+                }
+                if (deltaA[2] > deltaB[2]) {
+                    imageS.at<Vec3b>(i, j)[2] = imageA.at<Vec3b>(i, j)[2];
+                } else {
+                    imageS.at<Vec3b>(i, j)[2] = imageB.at<Vec3b>(i, j)[2];
+                }
+            }
+        }
+    }
+}
 
 // 将两个原图像的拉普拉斯金字塔融合
 void blendLaplacianPyramids(LapPyr& pyrA, LapPyr& pyrB, LapPyr& pyrS, Mat& dst) {
@@ -93,7 +141,11 @@ void blendLaplacianPyramids(LapPyr& pyrA, LapPyr& pyrB, LapPyr& pyrS, Mat& dst) 
     // 拉普拉斯金字塔各层分别融合
     for (int idx = 0; idx < pyrS.size(); idx++) {
         pyrS[idx] = pyrA[idx].clone();
-        blendLaplacianPyramidsByXYDir(pyrA[idx], pyrB[idx], pyrS[idx]);
+        if (idx == pyrS.size() - 1) {
+            blendLaplacianPyramidsByXYDir(pyrA[idx], pyrB[idx], pyrS[idx]);
+        } else {
+            blendLaplacianPyramidsByRE(pyrA[idx], pyrB[idx], pyrS[idx]);
+        }
     }
 
     // 输出图像
