26fe31bf6d18d500d02540fa632b29ce975a8df3
DLX <768606115@qq.com>
2020-02-01 21:21:33 +0800

    拉普拉斯金字塔图像融合

--- Demo/Pyramids.cpp
+++ Demo/Pyramids.cpp
@@ -14,6 +14,235 @@ using namespace cv;
 #define IMG2_PATH "D:\\code\\dan-qt\\Demo\\test2.bmp"
 #define AVATAR1_PATH "D:\\code\\dan-qt\\Demo\\avatar.jpg"
 #define AVATAR2_PATH "D:\\code\\dan-qt\\Demo\\avatar2.jpg"
+// 将mat输出到文件
+void writeMatToFile(Mat& m, const char* filename) {
+    ofstream fout(filename);
+
+    if (!fout) {
+        std::cout << "File Not Opened" << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            fout << m.at<Vec3b>(i, j) << "\t";
+        }
+        fout << std::endl;
+    }
+    fout.close();
+}
+
+// 浮点图像
+void writeMatToFile2(Mat& m, const char* filename) {
+    ofstream fout(filename);
+
+    if (!fout) {
+        std::cout << "File Not Opened" << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            fout << m.at<Vec3f>(i, j) << "\t";
+        }
+        fout << std::endl;
+    }
+    fout.close();
+}
+
+// 完美反射  白平衡算法
+Mat PerfectReflectionAlgorithm(Mat src) {
+    int row = src.rows;
+    int col = src.cols;
+    Mat dst(row, col, CV_8UC3);
+    int HistRGB[767] = { 0 };
+    int MaxVal = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            MaxVal = max(MaxVal, (int)src.at<Vec3b>(i, j)[0]);
+            MaxVal = max(MaxVal, (int)src.at<Vec3b>(i, j)[1]);
+            MaxVal = max(MaxVal, (int)src.at<Vec3b>(i, j)[2]);
+            int sum = src.at<Vec3b>(i, j)[0] + src.at<Vec3b>(i, j)[1] + src.at<Vec3b>(i, j)[2];
+            HistRGB[sum]++;
+        }
+    }
+    int Threshold = 0;
+    int sum = 0;
+    for (int i = 766; i >= 0; i--) {
+        sum += HistRGB[i];
+        if (sum > row * col * 0.1) {
+            Threshold = i;
+            break;
+        }
+    }
+    int AvgB = 0;
+    int AvgG = 0;
+    int AvgR = 0;
+    int cnt = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int sumP = src.at<Vec3b>(i, j)[0] + src.at<Vec3b>(i, j)[1] + src.at<Vec3b>(i, j)[2];
+            if (sumP > Threshold) {
+                AvgB += src.at<Vec3b>(i, j)[0];
+                AvgG += src.at<Vec3b>(i, j)[1];
+                AvgR += src.at<Vec3b>(i, j)[2];
+                cnt++;
+            }
+        }
+    }
+    AvgB /= cnt;
+    AvgG /= cnt;
+    AvgR /= cnt;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int Blue = src.at<Vec3b>(i, j)[0] * MaxVal / AvgB;
+            int Green = src.at<Vec3b>(i, j)[1] * MaxVal / AvgG;
+            int Red = src.at<Vec3b>(i, j)[2] * MaxVal / AvgR;
+            if (Red > 255) {
+                Red = 255;
+            }
+            else if (Red < 0) {
+                Red = 0;
+            }
+            if (Green > 255) {
+                Green = 255;
+            }
+            else if (Green < 0) {
+                Green = 0;
+            }
+            if (Blue > 255) {
+                Blue = 255;
+            }
+            else if (Blue < 0) {
+                Blue = 0;
+            }
+            dst.at<Vec3b>(i, j)[0] = Blue;
+            dst.at<Vec3b>(i, j)[1] = Green;
+            dst.at<Vec3b>(i, j)[2] = Red;
+        }
+    }
+    return dst;
+}
+
+// 直方图均衡
+Mat histogramCal(const Mat& image) {
+    int histSize = 255;                //直方图的最大像素值
+    float range[] = { 0,256 };
+    const float* histRange = { range };
+    vector<Mat> bgr;                   //存储图像的矩阵
+    split(image, bgr);                 //将彩色图像分割成，b,g,r分别存储
+    bool uniform = true, accumulate = false;
+    Mat b_hist, g_hist, r_hist;
+    //分别计算各个波段的直方图
+    calcHist(&bgr[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+    calcHist(&bgr[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+    calcHist(&bgr[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
+
+    //绘制直方图
+    int hist_w = 512, hist_h = 400;
+    int bin_w = cvRound((double)hist_w / histSize);
+
+    Mat histImage(hist_h, hist_w, CV_8U, Scalar(0, 0, 0));
+    //将结果归一化[0,histImage.rows]
+    normalize(b_hist,b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+
+    for (int i = 1; i < histSize; i++) {
+        line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+            Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))), Scalar(255, 0, 0));
+        line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+            Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))), Scalar(0, 255, 0));
+        line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+            Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))), Scalar(0, 0, 255));
+    }
+    return histImage;
+}
+
+//  CV_32FC3 转 CV_8UC3
+void convertTo8UC3(Mat& imageFrom, Mat& imageTo) {
+    int height = imageFrom.rows;
+    int width = imageFrom.cols;
+
+    // 方法3
+    double min = 0.0;
+    double max = 1.0;
+    minMaxIdx(imageFrom, &min, &max);
+
+    // 方法1
+    // normalize(imageFrom, imageFrom, 0, 1, NORM_L2);
+
+    // 方法2
+    // imageFrom *= 128;
+    // imageFrom += 200;
+
+    for (int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            for (int rgb = 0; rgb < 3; rgb++) {
+                double value = (imageFrom.at<Vec3f>(i, j)[rgb] - min) * 255 / (max - min);
+                // double value = (imageFrom.at<Vec3f>(i, j)[rgb]);
+
+                if ( value > 255) {
+                    imageTo.at<Vec3b>(i, j)[rgb] = 255;
+                } else if (value < 0) {
+                    imageTo.at<Vec3b>(i, j)[rgb] = 0;
+                } else {
+                    imageTo.at<Vec3b>(i, j)[rgb] = (uchar)value;
+                }
+
+            }
+        }
+    }
+}
+Mat singleScaleRetinex(const Mat& img, double sigma) {
+    Mat mat;
+    img.convertTo(mat, CV_32F);
+    Mat gauss(img.size(), img.type());
+    GaussianBlur(img, gauss, Size(0, 0), sigma);
+    gauss.convertTo(gauss, CV_32F);
+
+    /**
+     * retinex = np.log10(img) - np.log10(cv2.GaussianBlur(img, (0, 0), sigma))
+     * &
+     * lg - log_10
+     * ln - log_e
+     * lg(a) = ln(a) / ln(10)
+     */
+    log(mat, mat);
+    log(gauss, gauss);
+    auto den = log(10);
+    mat -= gauss;
+    mat /= den;
+    // mat.convertTo(mat, img.type());
+    return mat;
+}
+
+// 图像增强
+void ssr(Mat& src, Mat& dst, double sigma) {
+    Mat srcFloat = Mat::zeros(src.size(), CV_32FC3);
+    Mat srcBlur = Mat::zeros(src.size(), src.type());
+    Mat srcBlurFloat = Mat::zeros(src.size(), CV_32FC3);
+    Mat result = Mat::zeros(src.size(), CV_32FC3);
+
+    // 源图像转换成浮点
+    src.convertTo(srcFloat, CV_32FC3);
+
+    // 转对数
+    srcFloat += 0.01;
+    log(srcFloat, srcFloat);
+
+    // 源图像滤波
+    GaussianBlur(src, srcBlur, Size(0, 0), sigma);
+    srcBlur.convertTo(srcBlurFloat, CV_32FC3);
+    srcBlurFloat += 0.01;
+    // 转对数
+    srcFloat += 0.01;
+    log(srcBlurFloat, srcBlurFloat);
+
+    result = (srcFloat - srcBlurFloat) / log(10);
+    imshow("float", result);
+    convertTo8UC3(result, dst);
+}
 
 using LapPyr = vector<Mat>;
 // 通过源图像构造拉普拉斯金字塔
@@ -242,24 +471,6 @@ void showLaplacianPyramids(LapPyr& pyr) {
     }
 }
 
-// 将mat输出到文件
-void writeMatToFile(Mat& m, const char* filename) {
-    ofstream fout(filename);
-
-    if (!fout) {
-        std::cout << "File Not Opened" << std::endl;
-        return;
-    }
-
-    for (int i = 0; i < m.rows; i++) {
-        for (int j = 0; j < m.cols; j++) {
-            fout << m.at<Vec3b>(i, j) << "\t";
-        }
-        fout << std::endl;
-    }
-    fout.close();
-}
-
 int main() {
     LapPyr LA;
     LapPyr LB;
@@ -269,7 +480,10 @@ int main() {
 
     // 图像A 拉普拉斯金字塔
     Mat srcA = imread(IMG1_PATH);
-    buildLaplacianPyramids(srcA, LA);
+    Mat srcASSR = Mat::zeros(srcA.size(), CV_8UC3);
+    ssr(srcA, srcASSR, 80);
+    imshow("ssr", srcASSR);
+    buildLaplacianPyramids(srcASSR, LA);
     // showLaplacianPyramids(LA);
 
     // 图像B 拉普拉斯金字塔
@@ -291,9 +505,9 @@ int main() {
     imshow("3", dst3);
     waitKey(0);
 
-    // writeMatToFile(dst1, "D:\\code\\dan-qt\\Demo\\dst1.txt");
-    // writeMatToFile(dst2, "D:\\code\\dan-qt\\Demo\\dst2.txt");
-    // writeMatToFile(dst3, "D:\\code\\dan-qt\\Demo\\dst3.txt");
+    writeMatToFile(dst1, "D:\\code\\dan-qt\\Demo\\dst1.txt");
+    writeMatToFile(dst2, "D:\\code\\dan-qt\\Demo\\dst2.txt");
+    writeMatToFile(dst3, "D:\\code\\dan-qt\\Demo\\dst3.txt");
 
     return 0;
 }
